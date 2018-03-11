4896a2f17f93e4ec2b54cbbfaea4e5676c9e6d88
Chikage <33163118+chiakge@users.noreply.github.com>
2018-03-12 00:13:38 +0800

    更新版本号

--- tcp.sh
+++ tcp.sh
@@ -5,12 +5,12 @@ export PATH
 #=================================================
 #	System Required: CentOS 6+,Debian7+,Ubuntu12+
 #	Description: BBR+BBR魔改版+Lotserver
-#	Version: 1.1.1
+#	Version: 1.1.2
 #	Author: 千影
 #	Blog: https://www.94ish.me/
 #=================================================
 
-sh_ver="1.1.1"
+sh_ver="1.1.2"
 github="raw.githubusercontent.com/chiakge/Linux-NetSpeed/master"
 
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
