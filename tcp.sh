8849585329a96a0b7b2c88291f798afcc79d5e15
cx9208 <305866366@qq.com>
2019-06-24 17:25:13 +0000

    change github

--- tcp.sh
+++ tcp.sh
@@ -11,7 +11,7 @@ export PATH
 #=================================================
 
 sh_ver="1.3.1"
-github="raw.githubusercontent.com/chiakge/Linux-NetSpeed/master"
+github="raw.githubusercontent.com/cx9208/Linux-NetSpeed/master"
 
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
