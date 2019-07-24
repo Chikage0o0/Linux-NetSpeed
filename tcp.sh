fd1c3b04080b5d9df87abed1574d4094ef6b671b
Chikage <33163118+chiakge@users.noreply.github.com>
2019-07-25 03:30:09 +0800

    优化lotserver加速参数

--- tcp.sh
+++ tcp.sh
@@ -209,7 +209,12 @@ startlotserver(){
 		apt-get update
 		apt-get install ethtool
 	fi
-	bash <(wget --no-check-certificate -qO- https://github.com/MoeClub/lotServer/raw/master/Install.sh) install
+	bash <(wget --no-check-certificate -qO- https://raw.githubusercontent.com/chiakge/lotServer/master/Install.sh) install
+	sed -i '/advinacc/d' /appex/etc/config
+	sed -i '/maxmode/d' /appex/etc/config
+	echo -e "advinacc=\"1\"
+maxmode=\"1\"">>/appex/etc/config
+	/appex/bin/lotServer.sh restart
 	start_menu
 }
 
