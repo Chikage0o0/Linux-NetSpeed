56258be6b7f3e1a06570d3b708ee56ba7b251856
dr <z540453380@gmail.com>
2019-12-12 09:59:08 +0800

    Update tcpx.sh
    bbrplus更新到4.14.158

--- tcpx.sh
+++ tcpx.sh
@@ -54,12 +54,12 @@ installbbr(){
 
 #安装BBRplus内核
 installbbrplus(){
-	kernel_version="4.14.129-bbrplus"
+	kernel_version="4.14.158-bbrplus"
 	if [[ "${release}" == "centos" ]]; then
 		wget -N --no-check-certificate https://${github}/bbrplus/${release}/${version}/kernel-${kernel_version}.rpm
 		yum install -y kernel-${kernel_version}.rpm
 		rm -f kernel-${kernel_version}.rpm
-		kernel_version="4.14.129_bbrplus" #fix a bug
+		kernel_version="4.14.158_bbrplus" #fix a bug
 	elif [[ "${release}" == "debian" || "${release}" == "ubuntu" ]]; then
 		mkdir bbrplus && cd bbrplus
 		wget -N --no-check-certificate http://${github}/bbrplus/debian-ubuntu/${bit}/linux-headers-${kernel_version}.deb
