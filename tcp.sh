59f6fb8927edd29ba3c1b6e0e90e4706b0177074
dr <z540453380@gmail.com>
2022-02-10 11:41:45 +0800

    Update tcp.sh
    cn

--- tcp.sh
+++ tcp.sh
@@ -50,7 +50,7 @@ check_cn() {
   if [[ "$geoip" != "" ]]; then
     # echo "下面使用fastgit.org的加速服务"
     # echo ${1//github.com/download.fastgit.org}
-	echo https://endpoint.fastgit.org/$1
+    echo https://endpoint.fastgit.org/$1
   else
     echo $1
   fi
@@ -127,10 +127,10 @@ installbbr() {
       imgurl=$(curl -s 'https://api.github.com/repos/ylx2016/kernel/releases' | grep ${github_tag} | grep 'deb' | grep -v 'headers' | grep -v 'devel' | awk -F '"' '{print $4}')
       #headurl=https://github.com/ylx2016/kernel/releases/download/$github_tag/linux-headers-${github_ver}_${github_ver}-1_amd64.deb
       #imgurl=https://github.com/ylx2016/kernel/releases/download/$github_tag/linux-image-${github_ver}_${github_ver}-1_amd64.deb
-      
+
       headurl=$(check_cn $headurl)
       imgurl=$(check_cn $imgurl)
-	  echo -e "正在检查headers下载连接...."
+      echo -e "正在检查headers下载连接...."
       checkurl $headurl
       echo -e "正在检查内核下载连接...."
       checkurl $imgurl
@@ -419,13 +419,13 @@ installxanmod() {
       # sourceforge_xanmod_cacule_ver=$(curl -s https://sourceforge.net/projects/xanmod/files/releases/cacule/ | grep 'class="folder ">' | head -n 1 | awk -F '"' '{print $2}')
       # sourceforge_xanmod_cacule_file_img=$(curl -s https://sourceforge.net/projects/xanmod/files/releases/cacule/${sourceforge_xanmod_cacule_ver}/ | grep 'linux-image' | head -n 1 | awk -F '"' '{print $2}')
       # sourceforge_xanmod_cacule_file_head=$(curl -s https://sourceforge.net/projects/xanmod/files/releases/cacule/${sourceforge_xanmod_cacule_ver}/ | grep 'linux-headers' | head -n 1 | awk -F '"' '{print $2}')
-	  echo -e "如果下载地址出错，可能当前正在更新，超过半天还是出错请反馈，大陆自行解决污染问题"
+      echo -e "如果下载地址出错，可能当前正在更新，超过半天还是出错请反馈，大陆自行解决污染问题"
       github_tag=$(curl -s 'https://api.github.com/repos/ylx2016/kernel/releases' | grep 'Ubuntu_Kernel' | grep '_lts_latest_' | grep 'xanmod' | head -n 1 | awk -F '"' '{print $4}' | awk -F '[/]' '{print $8}')
       github_ver=$(curl -s 'https://api.github.com/repos/ylx2016/kernel/releases' | grep ${github_tag} | grep 'deb' | grep 'headers' | awk -F '"' '{print $4}' | awk -F '[/]' '{print $9}' | awk -F '[-]' '{print $3}')
       #echo -e "获取的xanmod lts版本号为:${sourceforge_xanmod_lts_ver}"
-	  echo -e "获取的xanmod lts版本号为:${github_ver}"
+      echo -e "获取的xanmod lts版本号为:${github_ver}"
       # kernel_version=$sourceforge_xanmod_stable_ver
-	  kernel_version=$github_ver
+      kernel_version=$github_ver
       # detele_kernel_head
       # headurl=https://sourceforge.net/projects/xanmod/files/releases/stable/${sourceforge_xanmod_stable_ver}/${sourceforge_xanmod_stable_file_head}/download
       # imgurl=https://sourceforge.net/projects/xanmod/files/releases/stable/${sourceforge_xanmod_stable_ver}/${sourceforge_xanmod_stable_file_img}/download
@@ -435,9 +435,11 @@ installxanmod() {
       #imgurl=https://sourceforge.net/projects/xanmod/files/releases/cacule/${sourceforge_xanmod_cacule_ver}/${sourceforge_xanmod_cacule_file_img}/download
       #headurl=https://sourceforge.net/projects/xanmod/files/releases/lts/${sourceforge_xanmod_lts_ver}/${sourceforge_xanmod_lts_file_head}/download
       #imgurl=https://sourceforge.net/projects/xanmod/files/releases/lts/${sourceforge_xanmod_lts_ver}/${sourceforge_xanmod_lts_file_img}/download
-	  headurl=$(curl -s 'https://api.github.com/repos/ylx2016/kernel/releases' | grep ${github_tag} | grep 'deb' | grep 'headers' | awk -F '"' '{print $4}')
+      headurl=$(curl -s 'https://api.github.com/repos/ylx2016/kernel/releases' | grep ${github_tag} | grep 'deb' | grep 'headers' | awk -F '"' '{print $4}')
       imgurl=$(curl -s 'https://api.github.com/repos/ylx2016/kernel/releases' | grep ${github_tag} | grep 'deb' | grep -v 'headers' | grep -v 'devel' | awk -F '"' '{print $4}')
 
+      headurl=$(check_cn $headurl)
+      imgurl=$(check_cn $imgurl)
       echo -e "正在检查headers下载连接...."
       checkurl $headurl
       echo -e "正在检查内核下载连接...."
@@ -1029,7 +1031,7 @@ net.ipv4.tcp_max_syn_backlog = 262144
 net.netfilter.nf_conntrack_max = 262144
 net.nf_conntrack_max = 262144
 EOF
-  sysctl -p	
+  sysctl -p
   sysctl --system
   echo madvise >/sys/kernel/mm/transparent_hugepage/enabled
 
@@ -1552,7 +1554,7 @@ check_sys() {
         Var_VirtType="dedicated"
         virtual="None"
         local Var_BIOSVendor
-		Var_BIOSVendor="$(dmidecode -s bios-vendor)"
+        Var_BIOSVendor="$(dmidecode -s bios-vendor)"
         if [ "${Var_BIOSVendor}" = "SeaBIOS" ]; then
           Var_VirtType="Unknown"
           virtual="Unknown with SeaBIOS BIOS"
@@ -1573,7 +1575,7 @@ check_sys() {
     else # 正常判断流程
       Var_VirtType="$(virt-what | xargs)"
       local Var_VirtTypeCount
-	  Var_VirtTypeCount="$(echo $Var_VirtTypeCount | wc -l)"
+      Var_VirtTypeCount="$(echo $Var_VirtTypeCount | wc -l)"
       if [ "${Var_VirtTypeCount}" -gt "1" ]; then # 处理嵌套虚拟化
         virtual="echo ${Var_VirtType}"
         Var_VirtType="$(echo ${Var_VirtType} | head -n1)" # 使用检测到的第一种虚拟化继续做判断
@@ -1581,7 +1583,7 @@ check_sys() {
         virtual="${Var_VirtType}"
       else
         local Var_BIOSVendor
-		Var_BIOSVendor="$(dmidecode -s bios-vendor)"
+        Var_BIOSVendor="$(dmidecode -s bios-vendor)"
         if [ "${Var_BIOSVendor}" = "SeaBIOS" ]; then
           Var_VirtType="Unknown"
           virtual="Unknown with SeaBIOS BIOS"
